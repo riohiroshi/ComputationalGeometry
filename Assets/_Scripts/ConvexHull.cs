26fbab3546e6a5fc7e13a315106b48df2565cb88
Rio Luo <riozhluo@gmail.com>
2022-08-08 16:20:19 +0800

    Refactory and Implement Quick Hull

--- Assets/_Scripts/ConvexHull.cs
+++ Assets/_Scripts/ConvexHull.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEditor;
 
 using NaughtyAttributes;
+using System.Linq;
 
 [ExecuteInEditMode]
 public class ConvexHull : MonoBehaviour
@@ -53,6 +54,16 @@ public class ConvexHull : MonoBehaviour
         _ballArray = _ballList.ToArray();
     }
 
+    private void ResetBallList()
+    {
+        _ballList = new List<Transform>();
+
+        for (int i = 0; i < _ballArray.Length; i++)
+        {
+            _ballList.Add(_ballArray[i]);
+        }
+    }
+
     [Button]
     private void StartGrahamScan()
     {
@@ -61,6 +72,8 @@ public class ConvexHull : MonoBehaviour
 
     private IEnumerator GrahamScan()
     {
+        ResetBallList();
+
         var bottomLeftPoint = FindBottomLeftPoint();
 
         SortPoints();
@@ -107,17 +120,17 @@ public class ConvexHull : MonoBehaviour
         {
             _ballList.Remove(bottomLeftPoint);
 
-            QuickSort(_ballList, 0, _ballList.Count - 1);
+            QuickSort(_ballList, 0, _ballList.Count - 1, CompareAngle);
 
             _ballList.Insert(0, bottomLeftPoint);
 
             _ballArray = _ballList.ToArray();
         }
 
-        bool CompareAngle(Vector3 point0, Vector3 point1)
+        bool CompareAngle(Transform point0, Transform point1)
         {
-            var line0 = point0 - bottomLeftPoint.position;
-            var line1 = point1 - bottomLeftPoint.position;
+            var line0 = point0.position - bottomLeftPoint.position;
+            var line1 = point1.position - bottomLeftPoint.position;
 
             var y = Vector3.Cross(line0, line1).y;
 
@@ -125,40 +138,6 @@ public class ConvexHull : MonoBehaviour
             if (y == 0f && (line0.sqrMagnitude < line1.sqrMagnitude)) { return true; }
 
             return false;
-        }
-
-        List<Transform> QuickSort(List<Transform> list, int left, int right)
-        {
-            if (left >= right) { return list; }
-
-            int partitionIndex = Partition(list, left, right);
-            QuickSort(list, left, partitionIndex - 1);
-            QuickSort(list, partitionIndex + 1, right);
-
-            return list;
-        }
-
-        int Partition(List<Transform> list, int left, int right)
-        {
-            int pivot = left;
-            int index = pivot + 1;
-            for (int i = index; i <= right; i++)
-            {
-                if (CompareAngle(list[i].position, list[pivot].position))
-                {
-                    Swap(list, i, index);
-                    index++;
-                }
-            }
-            Swap(list, pivot, index - 1);
-            return index - 1;
-        }
-
-        void Swap(List<Transform> list, int i, int j)
-        {
-            var temp = list[i];
-            list[i] = list[j];
-            list[j] = temp;
         }
 
         bool IsOnRight(Vector3 nextPoint, Vector3 lastPoint, Vector3 last2Point)
@@ -178,6 +157,8 @@ public class ConvexHull : MonoBehaviour
 
     private IEnumerator AndrewMonotoneChain()
     {
+        ResetBallList();
+
         SortPoints();
 
         _convexHullPoints = new List<Transform>();
@@ -196,7 +177,7 @@ public class ConvexHull : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
         }
 
-        var t = _convexHullPoints.Count-1;
+        var t = _convexHullPoints.Count - 1;
 
         for (int i = _ballArray.Length - 2; i >= 0; i--)
         {
@@ -222,51 +203,17 @@ public class ConvexHull : MonoBehaviour
 
         void SortPoints()
         {
-            QuickSort(_ballList, 0, _ballList.Count - 1);
+            QuickSort(_ballList, 0, _ballList.Count - 1, Compare);
 
             _ballArray = _ballList.ToArray();
         }
 
-        bool Compare(Vector3 point0, Vector3 point1)
+        bool Compare(Transform point0, Transform point1)
         {
-            if (point0.x < point1.x) { return true; }
-            if (point0.x == point1.x && point0.z < point1.z) { return true; }
+            if (point0.position.x < point1.position.x) { return true; }
+            if (point0.position.x == point1.position.x && point0.position.z < point1.position.z) { return true; }
 
             return false;
-        }
-
-        List<Transform> QuickSort(List<Transform> list, int left, int right)
-        {
-            if (left >= right) { return list; }
-
-            int partitionIndex = Partition(list, left, right);
-            QuickSort(list, left, partitionIndex - 1);
-            QuickSort(list, partitionIndex + 1, right);
-
-            return list;
-        }
-
-        int Partition(List<Transform> list, int left, int right)
-        {
-            int pivot = left;
-            int index = pivot + 1;
-            for (int i = index; i <= right; i++)
-            {
-                if (Compare(list[i].position, list[pivot].position))
-                {
-                    Swap(list, i, index);
-                    index++;
-                }
-            }
-            Swap(list, pivot, index - 1);
-            return index - 1;
-        }
-
-        void Swap(List<Transform> list, int i, int j)
-        {
-            var temp = list[i];
-            list[i] = list[j];
-            list[j] = temp;
         }
 
         bool IsOnRight(Vector3 nextPoint, Vector3 lastPoint, Vector3 last2Point)
@@ -286,6 +233,8 @@ public class ConvexHull : MonoBehaviour
 
     private IEnumerator Melkman()
     {
+        ResetBallList();
+
         _convexHullPoints = new List<Transform>();
         _convexHullPoints.Add(_ballArray[2]);
         _convexHullPoints.Add(_ballArray[2]);
@@ -310,10 +259,6 @@ public class ConvexHull : MonoBehaviour
             _ballArray[i].GetComponent<Renderer>().material.color = Color.blue;
 
             var last = _convexHullPoints.Count - 1;
-            var bp0 = _convexHullPoints[0].position;
-            var bp1 = _convexHullPoints[1].position;
-            var tp0 = _convexHullPoints[last].position;
-            var tp1 = _convexHullPoints[last - 1].position;
             var currentPoint = _ballArray[i].position;
 
             if (IsInsideConvex(currentPoint)) { continue; }
@@ -362,6 +307,259 @@ public class ConvexHull : MonoBehaviour
         }
     }
 
+    [Button]
+    private void StartQuickHull()
+    {
+        StartCoroutine(QuickHull());
+    }
+
+    private IEnumerator QuickHull()
+    {
+        ResetBallList();
+
+        FindPoints(out var p1, out var p2);
+        _convexHullPoints.Add(p1);
+        _convexHullPoints.Add(p2);
+
+        yield return new WaitForSeconds(1f);
+
+        var p3 = FindFurthestPointFromEdge(p1, p2, _ballList);
+        _convexHullPoints.Add(p3);
+
+        ClockwisePoints(ref p1, ref p2, ref p3);
+
+        _ballList.Remove(p1);
+        _ballList.Remove(p2);
+        _ballList.Remove(p3);
+
+        yield return new WaitForSeconds(1f);
+
+        var edge_3_1 = new List<Transform>();
+        var edge_2_3 = new List<Transform>();
+        var edge_1_2 = new List<Transform>();
+
+        RemovePointsWithinTriangle(p1.position, p2.position, p3.position, _ballList);
+
+        for (int i = _ballList.Count - 1; i >= 0; i--)
+        {
+            var point = _ballList[i].position;
+
+            var cross12 = (int)Mathf.Sign(Cross(p1.position, p2.position, point));
+            var cross23 = (int)Mathf.Sign(Cross(p2.position, p3.position, point));
+            var cross31 = (int)Mathf.Sign(Cross(p3.position, p1.position, point));
+
+            if (cross12 != cross23 && cross23 == cross31)
+            {
+                edge_1_2.Add(_ballList[i]);
+            }
+
+            if (cross23 != cross31 && cross31 == cross12)
+            {
+                edge_2_3.Add(_ballList[i]);
+            }
+
+            if (cross31 != cross12 && cross12 == cross23)
+            {
+                edge_3_1.Add(_ballList[i]);
+            }
+        }
+
+        var pointsOnHull_lf = CreateSubConvexHUll(p1, p3, edge_3_1);
+        var pointsOnHull_fr = CreateSubConvexHUll(p3, p2, edge_2_3);
+        var pointsOnHull_rl = CreateSubConvexHUll(p2, p1, edge_1_2);
+
+        _convexHullPoints.Clear();
+        _convexHullPoints.AddRange(pointsOnHull_lf);
+
+        yield return new WaitForSeconds(1f);
+
+        _convexHullPoints.AddRange(pointsOnHull_fr);
+
+        yield return new WaitForSeconds(1f);
+
+        _convexHullPoints.AddRange(pointsOnHull_rl);
+
+        void FindPoints(out Transform p1, out Transform p2)
+        {
+            p1 = default;
+            p2 = default;
+
+            var minX = float.PositiveInfinity;
+            var maxX = float.NegativeInfinity;
+
+            foreach (var ball in _ballArray)
+            {
+                if (ball.position.x < minX)
+                {
+                    minX = ball.position.x;
+                    p1 = ball;
+                }
+
+                if (ball.position.x > maxX)
+                {
+                    maxX = ball.position.x;
+                    p2 = ball;
+                }
+            }
+        }
+
+        Transform FindFurthestPointFromEdge(Transform p1, Transform p2, List<Transform> points)
+        {
+            var edge = p2.position - p1.position;
+
+            var edgeNormal = new Vector3(edge.z, 0f, -edge.x);
+
+            var maxDist = float.NegativeInfinity;
+
+            Transform p3 = default;
+
+            foreach (var ball in points)
+            {
+                var lb = ball.position - p1.position;
+
+                var dot = Mathf.Abs(Vector3.Dot(lb, edgeNormal));
+
+                if (dot > maxDist)
+                {
+                    maxDist = dot;
+                    p3 = ball;
+                }
+            }
+
+            return p3;
+        }
+
+        void ClockwisePoints(ref Transform p1, ref Transform p2, ref Transform p3)
+        {
+            var p1Pos = p1.position;
+            var p2Pos = p2.position;
+            var p3Pos = p3.position;
+
+            if (IsPointBOnRightOfLineOA(p1Pos, p2Pos, p3Pos)) { return; }
+
+            var temp = p3;
+            p3 = p2;
+            p2 = temp;
+        }
+
+        List<Transform> CreateSubConvexHUll(Transform p1, Transform p3, List<Transform> points)
+        {
+            if (points.Count == 0) { return new List<Transform>() { p1 }; }
+
+            var p2 = FindFurthestPointFromEdge(p1, p3, points);
+
+            points.Remove(p2);
+
+            RemovePointsWithinTriangle(p1.position, p2.position, p3.position, points);
+
+            if (points.Count == 0) { return new List<Transform>() { p1, p2 }; }
+
+            var edge_12 = new List<Transform>();
+            var edge_23 = new List<Transform>();
+
+            for (int i = points.Count - 1; i >= 0; i--)
+            {
+                var p = points[i];
+
+                if (IsPointBOnRightOfLineOA(p1.position, p2.position, p.position))
+                {
+                    edge_12.Add(p);
+                    continue;
+                }
+
+                edge_23.Add(p);
+            }
+
+            var subConvexHull_12 = CreateSubConvexHUll(p1, p2, edge_12);
+            var subConvexHull_23 = CreateSubConvexHUll(p2, p3, edge_23);
+
+            subConvexHull_12.AddRange(subConvexHull_23);
+
+            return subConvexHull_12;
+        }
+
+        void RemovePointsWithinTriangle(Vector3 p1, Vector3 p2, Vector3 p3, List<Transform> points)
+        {
+            for (int i = points.Count - 1; i >= 0; i--)
+            {
+                var point = points[i].position;
+
+                var cross12 = (int)Mathf.Sign(Cross(p1, p2, point));
+                var cross23 = (int)Mathf.Sign(Cross(p2, p3, point));
+                var cross31 = (int)Mathf.Sign(Cross(p3, p1, point));
+
+                if (cross12 != cross23 || cross12 != cross31 || cross23 != cross31) { continue; }
+
+                points[i].GetComponent<Renderer>().material.color = Color.blue;
+
+                points.RemoveAt(i);
+            }
+        }
+
+        float Cross(Vector3 o, Vector3 a, Vector3 b) => Vector3.Cross(a - o, b - o).y;
+
+        bool IsPointBOnRightOfLineOA(Vector3 o, Vector3 a, Vector3 b) => Cross(o, a, b) > 0f;
+
+        yield return null;
+    }
+
+    private List<T> QuickSort<T>(List<T> list, int left, int right, System.Func<T, T, bool> Compare)
+    {
+        if (left >= right) { return list; }
+
+        var partitionIndex = Partition(list, left, right, Compare);
+        QuickSort(list, left, partitionIndex - 1, Compare);
+        QuickSort(list, partitionIndex + 1, right, Compare);
+
+        return list;
+    }
+
+    private int Partition<T>(List<T> list, int left, int right, System.Func<T, T, bool> Compare)
+    {
+        var pivot = left;
+        var index = pivot + 1;
+        for (int i = index; i <= right; i++)
+        {
+            if (Compare(list[i], list[pivot]))
+            {
+                Swap(list, i, index);
+                index++;
+            }
+        }
+
+        Swap(list, pivot, index - 1);
+        return index - 1;
+    }
+
+    private void Swap<T>(List<T> list, int i, int j)
+    {
+        if (i >= list.Count || i < 0) { return; }
+        if (j >= list.Count || j < 0) { return; }
+
+        var temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
+    }
+
+    private static void PolygonInner(List<Vector3> points)
+    {
+        var m = new Mesh();
+        m.SetVertices(points);
+        m.SetNormals(points.Select(p => Vector3.up).ToList());
+
+        var triList = new List<int[]>();
+        for (int i = 2; i < points.Count; i++)
+        {
+            triList.Add(new int[] { 0, i - 1, i });
+        }
+
+        m.SetTriangles(
+                triList.SelectMany(tl => tl.ToList()).ToArray(),
+                0);
+
+        Gizmos.DrawMesh(m, Vector3.zero, Quaternion.identity, Vector3.one);
+    }
+
     private void OnDrawGizmos()
     {
         //Gizmos.DrawWireCube(transform.position, new Vector3(_generateBound.x * 2, 0.1f, _generateBound.y * 2));
@@ -380,5 +578,9 @@ public class ConvexHull : MonoBehaviour
         }
 
         Gizmos.DrawLine(_convexHullPoints[_convexHullPoints.Count - 1].position, _convexHullPoints[0].position);
+
+        //PolygonInner(_convexHullPoints.Select(p => p.position).ToList());
+        //var reverse = _convexHullPoints.Reverse<Transform>();
+        //PolygonInner(reverse.Select(p => p.position).ToList());
     }
 }
